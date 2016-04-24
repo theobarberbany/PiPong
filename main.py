09c329f427f09f4f4be1d097ae4fc3c5e28377cd
Theo <theobarberbany@gmail.com>
2016-04-24 22:22:42 +0100

    Added drawboard function

--- main.py
+++ main.py
@@ -2,20 +2,19 @@ from blessed import Terminal
 
 t = Terminal()
 
-class Screen(object, fps):
-	
-	def __init__(self, fps):
-		self.framerate = fps
-	#Mutators
-	
-	def setFrameRate(self, fps):
-		self.framerate = fps
-	#Accessors
+def printBlockCenter():
+	return(t.center(t.white_on_black(' ')).rstrip())
 
-	def getFrameRate(self):
-		return(self.framerate)
-	def getHeight(self):
-		return(t.height)
-	def getWidth(self):
-		return(t.width)	
-		
+class screen(object):
+	
+	def __init__(self):
+		pass	
+	
+	def drawNet(self):
+		with t.fullscreen():
+			for i in range(0, t.height,2):
+				print(t.move_y(t.height - i) + printBlockCenter())
+		t.inkey()
+
+board = screen()
+board.drawNet()
