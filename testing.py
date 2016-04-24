527caa38d499273d542ad67a1d24342ab67a306e
Theo <theobarberbany@gmail.com>
2016-04-24 21:05:38 +0100

    Updated testing.py

--- testing.py
+++ testing.py
@@ -1,7 +1,17 @@
 from blessed import Terminal
-
+import sys,os
+import subprocess
+subprocess.Popen("\e[40m")
 t = Terminal()
 
 print(t.height)
 print(t.width)
+print(t.white_on_black('Blue skies!'))
+
+term = Terminal()
+with term.fullscreen():
+    blue()
+    print(term.move_y(term.height // 2) +
+          term.center(term.white_on_black('press any key')).rstrip())
+    term.inkey()
 
