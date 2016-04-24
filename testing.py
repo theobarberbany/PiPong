0f88a6de670873f01c6ff28ef9c99de588830f85
Theo <theobarberbany@gmail.com>
2016-04-24 21:56:22 +0100

    modified testing.py

--- testing.py
+++ testing.py
@@ -1,17 +1,35 @@
 from blessed import Terminal
 import sys,os
 import subprocess
-subprocess.Popen("\e[40m")
 t = Terminal()
+
 
 print(t.height)
 print(t.width)
 print(t.white_on_black('Blue skies!'))
+def printblock():
+    return(t.center(t.white_on_black(' ')).rstrip())
 
-term = Terminal()
-with term.fullscreen():
-    blue()
-    print(term.move_y(term.height // 2) +
-          term.center(term.white_on_black('press any key')).rstrip())
-    term.inkey()
+
+with t.fullscreen():
+    print(t.move_y(t.height - 2) + printblock()) 
+    print(t.move_y(t.height - 4) + printblock())
+    print(t.move_y(t.height - 6) + printblock())
+    print(t.move_y(t.height - 8) + printblock())
+    print(t.move_y(t.height - 10) + printblock())
+    print(t.move_y(t.height - 12) + printblock())
+    print(t.move_y(t.height - 14) + printblock())
+    print(t.move_y(t.height - 16) + printblock())
+    print(t.move_y(t.height - 18) + printblock())
+    print(t.move_y(t.height - 20) + printblock())
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    t.inkey()
 
