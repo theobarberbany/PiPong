391c7ef310a97ad2f0286e1e009e6bae6177ac7b
Theo Barber-Bany <theobarberbany@users.noreply.github.com>
2016-04-26 14:44:30 +0100

    Added ball motion

--- main.py
+++ main.py
@@ -1,20 +1,65 @@
 from blessed import Terminal
+import random
+import time
+import logging
+
+logging.basicConfig(filename = "game.log", level = logging.DEBUG, filemode = 'w')
 
 t = Terminal()
+print(t.fullscreen)
+print(t.hidden_cursor)
+print(t.clear)
+def printbl(x, y):
+	print(t.move_y(y) + t.move_x(x) + t.white_on_black(' '))
 
 def printBlockCenter():
-	return(t.center(t.white_on_black(' ')).rstrip())
+	return(t.center(t.white_on_black(' ')))
+class Game(object):
+	def __init__(self):
+		self.ball = Ball()
+		self.net = Net()
+	def drawGame(self):
+		self.ball.drawBall()
+		self.net.drawNet()
+	
+	def updateGame(self):
+		self.ball.updateBall()
+	def resetGame(self):
+		self.ball.resetBall()
 
-class screen(object):
+	def startGame(self):
+		self.resetGame()
+		while True:
+			self.drawGame()
+			self.updateGame()
+			time.sleep(1/30)
+			print(t.clear)
+
+class Net(object):
 	
 	def __init__(self):
 		pass	
 	
 	def drawNet(self):
-		with t.fullscreen():
-			for i in range(0, t.height,2):
-				print(t.move_y(t.height - i) + printBlockCenter())
-		t.inkey()
-
-board = screen()
-board.drawNet()
+		for i in range(0, t.height,2):
+			print(t.move_y(t.height - i) + printBlockCenter())
+			
+class Ball(object):
+	def __init__(self):
+		starting_velocities = [-1, 1]
+		self.x_vel = random.choice(starting_velocities)
+		self.y_vel = random.choice(starting_velocities)
+	def drawBall(self):
+		printbl(self.x_pos, self.y_pos)		
+	def resetBall(self):
+		self.y_pos = t.height // 2
+		self.x_pos = t.width // 2
+	def updateBall(self):
+		logging.debug(" Y velocity: " + str(self.y_vel))
+		logging.debug("X velocity: " + str(self.x_vel))
+		self.y_pos += self.y_vel
+		self.x_pos += self.x_vel
+		logging.debug("self.y_pos"+ str(self.y_pos))
+		logging.debug("self.x_pos" + str(self.x_pos))
+game = Game()
+game.startGame()
--- testing.py
+++ testing.py
@@ -4,32 +4,19 @@ import subprocess
 t = Terminal()
 
 
-print(t.height)
-print(t.width)
-print(t.white_on_black('Blue skies!'))
+
+def printtest(x,y):
+	print(t.move(x,y) + t.white_on_black('.'))
+
 def printblock():
-    return(t.center(t.white_on_black(' ')).rstrip())
+	return(t.center(t.white_on_black(' ')).rstrip())
 
-
+def printbl(x, y):
+	print(t.move_y(y) + t.move_x(x) + t.white_on_black(' ').rstrip())
 with t.fullscreen():
-    print(t.move_y(t.height - 2) + printblock()) 
-    print(t.move_y(t.height - 4) + printblock())
-    print(t.move_y(t.height - 6) + printblock())
-    print(t.move_y(t.height - 8) + printblock())
-    print(t.move_y(t.height - 10) + printblock())
-    print(t.move_y(t.height - 12) + printblock())
-    print(t.move_y(t.height - 14) + printblock())
-    print(t.move_y(t.height - 16) + printblock())
-    print(t.move_y(t.height - 18) + printblock())
-    print(t.move_y(t.height - 20) + printblock())
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    t.inkey()
+#	for i in range(0,t.height,2):
+#		print(t.move_y(t.height- i) + printblock())
+##
+	printtest(15,12)
+	
 
