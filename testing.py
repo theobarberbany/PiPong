940d2b3283e4d3c14667d1250ecd27802bb29284
Theo <theobarberbany@gmail.com>
2016-04-26 14:41:58 +0100

    Implemented Ball movement

--- testing.py
+++ testing.py
@@ -1,7 +1,22 @@
 from blessed import Terminal
-
+import sys,os
+import subprocess
 t = Terminal()
 
-print(t.height)
-print(t.width)
+
+
+def printtest(x,y):
+	print(t.move(x,y) + t.white_on_black('.'))
+
+def printblock():
+	return(t.center(t.white_on_black(' ')).rstrip())
+
+def printbl(x, y):
+	print(t.move_y(y) + t.move_x(x) + t.white_on_black(' ').rstrip())
+with t.fullscreen():
+#	for i in range(0,t.height,2):
+#		print(t.move_y(t.height- i) + printblock())
+##
+	printtest(15,12)
+	
 
