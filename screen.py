dd7b254acd49e385db07a72f9b5bd6d3a98c10dd
JorgenFreeman <oliverdolan@msn.com>
2016-05-01 23:31:06 +0100

    Added screen script

---
+++ screen.py
@@ -0,0 +1,66 @@
+import sys
+import time
+import random
+
+class screen (object):
+
+    BASE_COLOUR = 40            #40=BLACK, base colour for backgroun
+    
+    def __init__ (self, width, height):
+        self._width = width
+        self._height= height
+
+        self._rows = []
+
+        for i in range(height):
+            row = []
+            for j in range(width):
+                row.append(self.BASE_COLOUR)    #Create an array for every row, initialise to base colour value
+
+            self._rows.append(row)
+
+        self._print_screen()
+
+    def _update_pixel(self, x, y, colour):
+
+        sys.stdout.write(chr(27) + "[u")        #Load stored cursor position (Stored at initial print screen to avoid screen resizing issues
+        sys.stdout.write(chr(27) + "[" + str(self._height + 1) + "A")   #Offset this position so that top-left is (0,0) (Move cursor up by height +1)
+
+        sys.stdout.write(chr(27) + "[" + str(x) + "C")  #Move cursor forward by x
+        sys.stdout.write(chr(27) + "[" + str(y) + "B")  #Move cursor down by y
+
+        self.__print_block(colour)                      #print a single block in the current cursor position
+        sys.stdout.write(chr(27) + "[u")                #Restore the original cursor position
+        
+
+    def _print_screen(self):     #DO NOT call this every frame; too slow. Called once in initilisation
+        sys.stdout.write(chr(27) + "[2J")               #clear the screen
+        sys.stdout.write(chr(27) + "[s")                #Save default cursor position
+        for row in self._rows:
+            self.__print_line(row)                      #print each line
+
+    def __print_block(self, colour):
+        sys.stdout.write("\x1b[" + str(colour) + "m \x1b[0m")       #print a single block of given colour
+                                                                    #40=Black; 41=Red; 42=Green; 43=Yellow; 44=Blue; 45=Magenta; 46=Cyan; 47=White
+
+    def __print_line(self, line_arr):                               #print a line of blocks given by an array of ints
+
+        sys.stdout.write(chr(27) + "[" + str(1) + "C")              #Move the cursor one space forward (it starts in the "-1" slot for some reason)
+        
+        for i in range(0, len(line_arr)):                           
+            self.__print_block(line_arr[i])
+
+        sys.stdout.write('\n')                                      #New line
+        
+
+s = screen(80,10)
+
+colours = [40,41,42,43,44,45,46,47]
+
+while True:
+    c = random.choice(colours)
+    x = random.randrange(0, 81)
+    y = random.randrange(0, 11)
+    s._update_pixel(x, y, c)
+
+print("")
